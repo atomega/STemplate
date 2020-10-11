065e87e723e529e0618d1fc5a9357ecc436b9ee8
polymurph <eddyed.k@gmail.com>
2020-10-11 14:36:33 +0200

    work on hw_reg.h

--- HAL/CPP/CSL/hw_reg.h
+++ HAL/CPP/CSL/hw_reg.h
@@ -57,9 +57,23 @@ class HWReg
 			reg = value;
 		}
 
+		/**
+		 * @brief Sets a sub-value in the register.
+		 *
+		 * @param bitWidth bit width of the sub-value
+		 * @param shift shift for the sub-value
+		 *
+		 * @param value sub-value to be set
+		 */
+		 template<unsigned int bitWidth, unsigned int shift>
+		 void setSubValue(const RegType&  value)
+		 {
+			clearBits(((static_cast<RegType>(1) << bitWidth) - static_cast<RegType>(1)) << shift);
+			setBits(value << shift); 			 
+		 }
 
 	private:
-		
+	 		 
 };
 
 
