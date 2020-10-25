3b122dce972a588f4b26363076df26c143e39327
polymurph <eddyed.k@gmail.com>
2020-10-25 18:14:05 +0100

    work on Pin.h

--- hal/cpp/csl/pin.h
+++ hal/cpp/csl/pin.h
@@ -1,6 +1,40 @@
 #ifndef _PIN_H_
 #define _PIN_H_
 
+#include "bits.h"
+#include "HwReg.h"
+#include "Port.h"
 
+namespace csl{
+namespace pin{
+
+
+enum Mode{
+    INPUT_MODE,
+    GENERAL_PURPOUSE_OUTPUT_MODE,
+    ALTERNATE_FUNCTION_MODE,
+    ANALOG_MODE
+};
+
+enum {
+};
+
+
+
+/**
+* @brief The STM32 pin base class.
+*
+* @param id port ID
+*/
+template<port::Id id>
+class PinBase{
+    public:
+
+        PinBase();
+
+};
+
+}   // namespace pin
+}   // namespace csl
 
 #endif // _PIN_H_
