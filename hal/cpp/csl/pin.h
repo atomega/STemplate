5550d0daf0919fba96dfa93b33594d65df4117ca
Edwin Koch <eddyed.k@gmail.com>
2020-10-25 18:52:18 +0100

    work on modeling pin class

--- hal/cpp/csl/pin.h
+++ hal/cpp/csl/pin.h
@@ -5,10 +5,15 @@
 #include "HwReg.h"
 #include "Port.h"
 
+
+// TODO: implement lock feature -> datasheet 9.4.8
+
 namespace csl{
 namespace pin{
 
-
+/**
+ * @brief Pin Modes.
+ */
 enum Mode{
     INPUT_MODE,
     GENERAL_PURPOUSE_OUTPUT_MODE,
@@ -16,9 +21,51 @@ enum Mode{
     ANALOG_MODE
 };
 
-enum {
+/**
+ * @brief Pin Speeds
+ */
+enum Speed{
+    LOW,
+    MEDIUM,
+    HIGH,
+    VERY_HIGH
 };
 
+/**
+ * @brief Pin States.
+ */
+enum State{
+    FLOATING,
+    PULL_UP,
+    PULL_DOWN
+};
+
+/**
+ * @brief Pin Types.
+ */
+enum Type{
+    PUSH_PULL,
+    OPEN_DRAIN
+};
+
+enum AlternateFunction{
+    AF_0,
+    AF_1,
+    AF_2,
+    AF_3,
+    AF_4,
+    AF_5,
+    AF_6,
+    AF_7,
+    AF_8,
+    AF_9,
+    AF_10,
+    AF_11,
+    AF_12,
+    AF_13,
+    AF_14,
+    AF_15
+};
 
 
 /**
